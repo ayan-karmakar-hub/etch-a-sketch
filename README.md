77d7edb12c1cf11449914441c2ceb1d072a57967
Ayan Karmakar <ayan_karmakar@utexas.edu>
2020-05-02 00:41:01 -0500

    completed project and README

---
+++ README.md
@@ -0,0 +1,19 @@
+
+/********** Summary ***********/
+
+For this project, I created a simple sketch pad. The main goal was to
+familiarize myself more with Javascript. I utiized functions, event 
+listeners, and css styling to manipulate DOM nodes I created (all
+withing a JavaScript file).
+
+It certainly would have been a lot cleaner to split up the work between
+separate HTML, CSS, and JavaScript files, but I think this project gave 
+me a good understanding of exactly what JavaScript is capable of doing.
+
+/******** Relevant Files *********/
+
+index.html: A basic html skeleton. I did almost all the work in JavaScript;
+
+sketch.js: This file was the workhorse for this project. It added all the
+    html,css and javascript I needed to complete this assignment. It is
+    responsible for resetting the board and changing the square size.
---
+++ sketch.js
@@ -0,0 +1,80 @@
+
+//set up initial display
+let size = 4;
+createResetButton();
+createChangeSquareButton();
+createSketchPad();
+
+function createResetButton(){
+    // creating clear button
+    let reset = document.createElement('button');
+    reset.innerText="Reset Squares";
+    reset.style.cssText=" display: inline; margin-left:45vw; font-size:20px;"
+    document.body.appendChild(reset);
+
+    // register mouse click
+    reset.addEventListener('click', clearGrid);
+}
+
+function createChangeSquareButton(){
+    // creating clear button
+    let change = document.createElement('button');
+    change.innerText="Change Square Size";
+    change.style.cssText="display: inline;  font-size:20px;"
+    document.body.appendChild(change);
+
+    // register mouse click
+    change.addEventListener('click', changeSquareSize);
+
+}
+
+function createSketchPad(){
+    // creating the grid container
+    let container = document.createElement('div');
+    container.id="container";
+
+    container.style.cssText = `margin-left: 40vw; margin-top: 15vh;
+    height: 500px; width: 500px;  border: 10px solid black;
+        display: grid;
+        grid-template-columns: repeat(1fr,${size});
+        grid-template-rows: repeat(1fr,${size});`;
+
+    // creating the grid cells
+    for(let i = 1; i <= size; i++){
+        for(let j = 1; j <= size; j++){
+            let newDiv = document.createElement('div');
+            let cellNumber = (i-1)*size+j-1;
+
+            newDiv.classList.add('cell');
+            newDiv.id =`${cellNumber}`;
+            newDiv.style.cssText = `grid-area: cell${i};
+            grid-row: ${i}/${i+1}; grid-column: ${j}/${j+1};
+            background-color: white; border-width: 10px;
+            text-align:center;"`;
+
+            //register mouse movements
+            newDiv.addEventListener('mouseenter', colorChange);
+            container.appendChild(newDiv);
+        }
+    }
+    document.body.appendChild(container);
+}
+
+function colorChange(){
+    this.style.backgroundColor= "black";
+}
+
+function changeSquareSize(){
+    do{
+        size = +prompt("How many squares do you want each side to be?\n\
+For best performance choose a number less than 50.");
+    } while(isNaN(size))
+    clearGrid();
+}
+
+function clearGrid(){
+    let container = document.body.querySelector('#container');
+    document.body.removeChild(container);
+
+    createSketchPad();
+}
